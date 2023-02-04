a13a546c5778a9560d639824186fae31f1a01d54
Ahmao <suhsuan520@gmail.com>
2023-02-04 10:55:26 +0800

    Add files via upload

---
+++ SortOut_CString_Name.cpp
@@ -0,0 +1,12 @@
+﻿#include <iostream>
+#include"nameutils.h"
+
+int main(int argc, char* argv[])
+{
+	std::cout<<"[SpaceCheck]" << argv[1] << "[SpaceCheck]" << std::endl;
+	nameutile::capitalize(argv[1]);
+	nameutile::trim_midle_names(argv[1]);
+	std::cout << std::endl;
+	std::cout << "[SpaceCheck]" << argv[1] << "[SpaceCheck]" << std::endl;
+}
+
---
+++ nameutils.h
@@ -0,0 +1,13 @@
+#pragma once
+namespace nameutile
+{
+	void capitalize(char argv[]);
+	void trim_midle_names(char argv[]);
+}
+int count(char ch[]);
+char capABCt(char c);
+char abcCapt(char c);
+int find_final(char argv[], int endIndex);
+void back_space(char ch[], int n, int a);
+void typing(char ch[], int in, int end, char ch_in);
+void space_delete(char argv[], int on);
